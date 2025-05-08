60133ab84890851e7da5649c6d10b141d6e94d00
kzhiguang <kzhiguang@infervision.com>
2025-05-08 18:08:02 +0800

    dev

--- src/PoetryPlanet/Services/PoetryService.cs
+++ src/PoetryPlanet/Services/PoetryService.cs
@@ -3,37 +3,45 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Json;
+using System.Text.Json;
 using PoetryPlanet.Dtos;
 
 namespace PoetryPlanet.Services;
 
 public class PoetryService
 {
-    private const string workListRoute = "/api/v1/work_list";
     private const string workRoute = "/api/v1/works";
-   private readonly HttpClient httpClient = new();
-   
-   public PoetryService()
-   {
-       httpClient.BaseAddress = new Uri("https://home.freemanke.com:60011");
-   }
-    
+    private const string workListRoute = "/api/v1/work_list";
+    private readonly string rootPath;
+    private readonly string workListFilePath;
+    private readonly HttpClient httpClient = new();
+
+    public PoetryService()
+    {
+        rootPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        workListFilePath = Path.Combine(rootPath, "work_list.json");
+        httpClient.BaseAddress = new Uri("https://home.freemanke.com:60011");
+
+        // 在IOS环境下，反序列化对象前，需要创建一个对象，否则会反序列化报错
+        var stamp = new WorkListItemInfo { Id = 10, Title = "", Author = "我", Dynasty = "" };
+        var work = new WorkInfo { Id = 10, Title = "标题", Author = "作者", Dynasty = "年代", Content = "内容" };
+    }
+
     public List<WorkListItemInfo> GetWorkList()
     {
         var works = new List<WorkListItemInfo>();
-        var stamp = new WorkListItemInfo
-            { Id = 10, Title = "", Author = "我", Dynasty = "", AuthorId = 0, AuthorRemoteId = "" };
-        Console.WriteLine($"在IOS环境下，反序列化对象前，需要创建一个对象，否则会反序列化报错：{stamp}");
+        if (TryGet<List<WorkListItemInfo>>(workListFilePath, out var workList)
+            && workList != null) return workList;
+
         try
         {
-            var docRoot = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             var request = new HttpRequestMessage(HttpMethod.Get, workListRoute);
             var response = httpClient.SendAsync(request).Result;
             var json = response.Content.ReadAsStringAsync().Result;
-            var infos = response.Content.ReadFromJsonAsync<List<WorkListItemInfo>>().Result;
-            var filePath = Path.Combine(docRoot, "works.json");
-            File.WriteAllText(filePath, json);
-            Console.WriteLine($"文件已保存到：{filePath}");
+            var infos = JsonSerializer.Deserialize<List<WorkListItemInfo>>(json);
+
+            File.WriteAllText(workListFilePath, json);
+            Console.WriteLine($"文件已保存到：{workListFilePath}");
             if (infos != null) works.AddRange(infos);
         }
         catch (Exception e)
@@ -43,29 +51,49 @@ public class PoetryService
 
         return works;
     }
-    
+
+    private bool TryGet<T>(string jsonFilePath, out T? value) where T : class
+    {
+        value = null;
+        if (File.Exists(jsonFilePath))
+        {
+            try
+            {
+                var json = File.ReadAllText(jsonFilePath);
+                var find = JsonSerializer.Deserialize<T>(json);
+                value = find;
+                return find != null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        return false;
+    }
+
     public WorkInfo GetWork(int id)
     {
-        var work = new WorkInfo
-            { Id = 10, Title = "标题", Author = "作者", Dynasty = "年代", AuthorId = 0, AuthorRemoteId = "",Content = "内容"};
-        Console.WriteLine($"在IOS环境下，反序列化对象前，需要创建一个对象，否则会反序列化报错：{work}");
+        var filePath = Path.Combine(rootPath, $"{id}.json");
+        if (TryGet<WorkInfo>(filePath, out var value)
+            && value != null) return value;
+
         try
         {
-            var docRoot = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
             var request = new HttpRequestMessage(HttpMethod.Get, workRoute + $"?id={id}");
             var response = httpClient.SendAsync(request).Result;
             var json = response.Content.ReadAsStringAsync().Result;
             var find = response.Content.ReadFromJsonAsync<WorkInfo>().Result;
-            var filePath = Path.Combine(docRoot, "work.json");
             File.WriteAllText(filePath, json);
             Console.WriteLine($"文件已保存到：{filePath}");
-            if (find != null) work = find;
+            if (find != null) return find;
         }
         catch (Exception e)
         {
             Console.WriteLine(e.Message);
         }
 
-        return work;
+        return new WorkInfo();
     }
 }
