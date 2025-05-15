28d9b8ae88b242f5d3ad5d4cb6f2c3f439617c31
kzhiguang <kzhiguang@infervision.com>
2025-05-15 16:04:36 +0800

    迭代开发

--- src/PoetryPlanet/ViewModels/SettingViewModel.cs
+++ src/PoetryPlanet/ViewModels/SettingViewModel.cs
@@ -1,7 +1,6 @@
-using Avalonia;
-using Avalonia.Styling;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Extensions.Logging;
 
 namespace PoetryPlanet.ViewModels;
 
@@ -10,8 +9,17 @@ public partial class SettingViewModel : ViewModelBase
     private readonly AppSetting appSetting;
     [ObservableProperty] private bool isDark;
 
+    /// <summary>
+    /// 默认构造方法，用于设计模式
+    /// </summary>
+    public SettingViewModel() : this(new AppSetting())
+    {
+        logger.LogInformation("使用默认构造方法");
+    }
+
     public SettingViewModel(AppSetting appSetting)
     {
+        logger.LogInformation($"使用带参数构造方法");
         this.appSetting = appSetting;
         IsDark = appSetting.IsDark;
     }
